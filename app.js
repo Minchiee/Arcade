12d389f705af039448e3016e6014aefbebd361da
Minh Nguyen <lambandwoof@gmail.com>
2022-10-09 21:58:35 -0500

    check for occupation in a cell

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const cell = document.querySelectorAll('.cell');
-const playerOne = document.querySelectorAll('.player1Score');
-const playerTwo = document.querySelectorAll('.player2Score');
-const restart = document.querySelectorAll('.restart');
+const playerOneScore = document.querySelector('.player1Score');
+const playerTwoScore = document.querySelector('.player2Score');
+const restart = document.querySelector('.restart');
+const playerOneName = prompt("What is Player One's name?");
+const playerTwoName = prompt("What is Player Two's name?");
 
 const winCon = [
     [0, 1, 2],
@@ -22,28 +24,75 @@ let score = {
     player2 : 0
 }
 
+function getScore() {
+    playerOneScore.innerHTML = `${playerOneName}: ${score.player1}`;
+    playerTwoScore.innerHTML = `${playerTwoName}: ${score.player2}`;
+}
+getScore();
+
+
 let flag = true;
 
 for(let i = 0; i < cell.length; i++){
     cell[i].addEventListener('click', () =>{
         if (flag === true){
-            cell[i].innerHTML = "X";
-            player1.push(i);
-            flag = false;
+            if (cell[i].innerHTML == ""){
+                cell[i].innerHTML = "X";
+                player1.push(i);
+                flag = false;
+            }
+            else {
+                alert("Cannot take move!");
+            }
         }
-        else{
-            cell[i].innerHTML = "O";
-            player2.push(i);
-            flag = true;
+        else {
+            if (cell[i].innerHTML == ""){
+                cell[i].innerHTML = "O";
+                player2.push(i);
+                flag = true;
+            }
+            else{
+                alert("Cannot take move!")
+            }
         }
+        isWinner();
     })
 }
 
 
-function isWinner(){
-    winCon.find
+function resetBoard(){
+    for(let i = 0; i< cell.length; i++){
+        cell[i].innerHTML = "";
+    }
+    player1 = [];
+    player2 = [];
+    flag = true;
 }
 
-const isOccupied = (tile) => {
+restart.addEventListener('click', () => {
+    resetBoard();
+})
 
+function isWinner(){
+    winCon.find((item) => {
+        if (item.filter((i) => player1.includes(i).length === 3)){
+            alert("Player 1 Wins!");
+            score.player1++;
+            getScore();
+            return item;
+        }
+        else if (item.filter((i) => player2.includes(i).length === 3)){
+            alert("Player 2 Wins!");
+            score.player2++;
+            getScore();
+        }
+        return;
+    })
+}
+
+
+function isOccupied(i){
+    if(cell[i].innerHTML != ""){
+        alert("Space is occupied")
+    }
 }

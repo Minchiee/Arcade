81dc2efa439fe64f82c658c10a598e92532fc2f3
Minh Nguyen <lambandwoof@gmail.com>
2022-10-09 22:14:10 -0500

    added documentation

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const cell = document.querySelectorAll('.cell');
 const playerOneScore = document.querySelector('.player1Score');
 const playerTwoScore = document.querySelector('.player2Score');
 const restart = document.querySelector('.restart');
+
+//Asks for name when starting website
 const playerOneName = prompt("What is Player One's name?");
 const playerTwoName = prompt("What is Player Two's name?");
 
+
+//Listed all the win conditions available
 const winCon = [
     [0, 1, 2],
     [0, 3, 6],
@@ -16,14 +20,17 @@ const winCon = [
     [6, 7, 8],
 ]
 
+//Set player1 and player2 empty and made their score 0
 let player1 = [];
 let player2 = [];
 
+//score.player1 or score.player2 will get their score
 let score = {
     player1 : 0,
     player2 : 0
 }
 
+//Displaying score
 function getScore() {
     playerOneScore.innerHTML = `${playerOneName}: ${score.player1}`;
     playerTwoScore.innerHTML = `${playerTwoName}: ${score.player2}`;
@@ -31,15 +38,17 @@ function getScore() {
 getScore();
 
 
-let flag = true;
+let turn = true;
 
 for(let i = 0; i < cell.length; i++){
     cell[i].addEventListener('click', () =>{
-        if (flag === true){
+        //if it's first person turn
+        if (turn === true){
             if (cell[i].innerHTML == ""){
                 cell[i].innerHTML = "X";
+                //adds what index you clicked on into the empty array of player1
                 player1.push(i);
-                flag = false;
+                turn = false;
             }
             else {
                 alert("Cannot take move!");
@@ -48,8 +57,9 @@ for(let i = 0; i < cell.length; i++){
         else {
             if (cell[i].innerHTML == ""){
                 cell[i].innerHTML = "O";
+                //adds what index you clicked on into the empty array of player2
                 player2.push(i);
-                flag = true;
+                turn = true;
             }
             else{
                 alert("Cannot take move!")
@@ -58,16 +68,17 @@ for(let i = 0; i < cell.length; i++){
     })
 }
 
-
+//function to reset the board
 function resetBoard(){
     for(let i = 0; i< cell.length; i++){
         cell[i].innerHTML = "";
     }
     player1 = [];
     player2 = [];
-    flag = true;
+    turn = true;
 }
 
+//reset the board when click the restart button
 restart.addEventListener('click', () => {
     resetBoard();
 })
@@ -89,9 +100,3 @@ function isWinner(){
     })
 }
 
-
-function isOccupied(i){
-    if(cell[i].innerHTML != ""){
-        alert("Space is occupied")
-    }
-}
